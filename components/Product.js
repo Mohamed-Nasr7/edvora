710e5555d3dcd0e374ec572a4b34137c94f880f5
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-01-02 16:57:48 +0200

    Give the product wrapper div a className container

--- components/Product.js
+++ components/Product.js
@@ -3,7 +3,7 @@ import productStyles from '../styles/Product.module.css';
 
 function Product({ productName, data }) {
   return (
-    <div>
+    <div className={productStyles.container}>
       <h3 className={productStyles.title}>{productName}</h3>
       <div>Cards</div>
     </div>
