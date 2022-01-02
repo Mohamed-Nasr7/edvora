fd2d8acd0aad9754e90bd87c46b9841b074b74df
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-01-02 16:33:56 +0200

    Add the className title to the product name h4 element

--- components/Product.js
+++ components/Product.js
@@ -4,7 +4,7 @@ import productStyles from '../styles/Product.module.css';
 function Product({ productName, data }) {
   return (
     <div>
-      <h4>{productName}</h4>
+      <h4 className={productStyles.title}>{productName}</h4>
       <div>Cards</div>
     </div>
   );
