976169523020d05b4e37904e9d63e8ea4f97e07b
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-01-02 22:06:42 +0200

    Loop through the data inside Product.js useEffect

--- components/Product.js
+++ components/Product.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from 'react';
 import productStyles from '../styles/Product.module.css';
 
 function Product({ productName, data }) {
+  useEffect(() => {
+    const productItems = data.map(product => {
+      if (product.product_name === productName) return <p>{productName}</p>;
+    });
+  }, []);
+
   return (
     <div className={productStyles.container}>
       <h3 className={productStyles.title}>{productName}</h3>
