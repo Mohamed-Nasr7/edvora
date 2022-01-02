9d92e8218d71e4ae1f59696c67a193c408878f4f
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-01-02 21:56:35 +0200

    Import hooks inside Product.js

--- components/Product.js
+++ components/Product.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import productStyles from '../styles/Product.module.css';
 
 function Product({ productName, data }) {
