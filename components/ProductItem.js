74ede90ada23264d9b76c486569cd5de9db04975
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-01-14 15:17:08 +0200

    Add a material ui card template inside the returned jsx from ProductItem component

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -9,7 +9,29 @@ import {
 } from '@mui/material';
 
 function ProductItem() {
-  return <div></div>;
+  return (
+    <Card>
+      <CardMedia
+        component='img'
+        height='140'
+        image='/static/images/cards/contemplative-reptile.jpg'
+        alt='green iguana'
+      />
+      <CardContent>
+        <Typography gutterBottom variant='h5' component='div'>
+          Lizard
+        </Typography>
+        <Typography variant='body2' color='text.secondary'>
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size='small'>Share</Button>
+        <Button size='small'>Learn More</Button>
+      </CardActions>
+    </Card>
+  );
 }
 
 export default ProductItem;
