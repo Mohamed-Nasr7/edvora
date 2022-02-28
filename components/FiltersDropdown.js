474b3d234e201989efc91aebe1ed5dedcdb705a5
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-02-28 19:05:40 +0200

    Import MaterialUI components and FiltersStyles css module inside FiltersDropdown.js

--- components/FiltersDropdown.js
+++ components/FiltersDropdown.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import FiltersStyles from '../styles/Filters.module.css';
+import { Button, Menu, MenuItem } from '@mui/material';
 
 function FiltersDropdown() {
   return <div></div>;
