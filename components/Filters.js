888770baffbd7da6d778a4c8011bb8f44d28c040
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-02-28 19:21:47 +0200

    Remove the JSX of the dropdown menu filters and the MUI imports from Filters.js

--- components/Filters.js
+++ components/Filters.js
@@ -1,6 +1,5 @@
 import React, { useReducer } from 'react';
 import FiltersStyles from '../styles/Filters.module.css';
-import { Button, Menu, MenuItem } from '@mui/material';
 
 function Filters() {
   const initialState = {
@@ -52,31 +51,6 @@ function Filters() {
     <div>
       <div className={FiltersStyles.container}>
         <p className={FiltersStyles.title}>Filters</p>
-        <Button
-          id='basic-button'
-          aria-controls={firstOpen ? 'basic-menu' : undefined}
-          aria-haspopup='true'
-          aria-expanded={firstOpen ? 'true' : undefined}
-          onClick={event => handleClick('FIRST_EL', event)}
-          className={FiltersStyles.dropdownBtn}
-        >
-          Products
-        </Button>
-        <Menu
-          id='basic-menu'
-          anchorEl={anchorElements.firstAnchorEl}
-          open={firstOpen}
-          onClose={() => handleClose('FIRST_EL')}
-          MenuListProps={{
-            'aria-labelledby': 'basic-button',
-          }}
-        >
-          <MenuItem onClick={() => handleClose('FIRST_EL')}>Profile</MenuItem>
-          <MenuItem onClick={() => handleClose('FIRST_EL')}>
-            My account
-          </MenuItem>
-          <MenuItem onClick={() => handleClose('FIRST_EL')}>Logout</MenuItem>
-        </Menu>
       </div>
     </div>
   );
