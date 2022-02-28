0bf11abf010574e8c07a7ed356f296fe90ca7951
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-02-28 19:12:20 +0200

    Add the Button and the Menu elements of the filters inside FiltersDropdown.js

--- components/FiltersDropdown.js
+++ components/FiltersDropdown.js
@@ -3,7 +3,33 @@ import FiltersStyles from '../styles/Filters.module.css';
 import { Button, Menu, MenuItem } from '@mui/material';
 
 function FiltersDropdown() {
-  return <div></div>;
+  return (
+    <div>
+      <Button
+        id='basic-button'
+        aria-controls={firstOpen ? 'basic-menu' : undefined}
+        aria-haspopup='true'
+        aria-expanded={firstOpen ? 'true' : undefined}
+        onClick={event => handleClick('FIRST_EL', event)}
+        className={FiltersStyles.dropdownBtn}
+      >
+        Products
+      </Button>
+      <Menu
+        id='basic-menu'
+        anchorEl={anchorElements.firstAnchorEl}
+        open={firstOpen}
+        onClose={() => handleClose('FIRST_EL')}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={() => handleClose('FIRST_EL')}>Profile</MenuItem>
+        <MenuItem onClick={() => handleClose('FIRST_EL')}>My account</MenuItem>
+        <MenuItem onClick={() => handleClose('FIRST_EL')}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
 }
 
 export default FiltersDropdown;
